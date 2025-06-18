85d8c5675697e48fcc7e4370888d2a4de2450700
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-06-18 16:18:28 +0400

    added Task model with foreign key to User

--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, Enum, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
 import enum
+
 
 class TaskStatus(enum.Enum):
     NEW = "New"
     IN_PROGRESS = "In progress"
     COMPLETED = "Completed"
+
 
 class User(Base):
     __tablename__ = "users"
@@ -15,3 +18,18 @@ class User(Base):
     last_name = Column(Text, nullable=True)
     username = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
+
+    tasks = relationship("Task", back_populates="owner",
+                         cascade="all, delete", passive_deletes=True)
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    status = Column(Enum(TaskStatus), default=TaskStatus.NEW, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    owner = relationship("User", back_populates="tasks")
