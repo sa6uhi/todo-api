8a5a42433d140eb552151cfb754dbb105c1dbfbf
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-06-18 14:51:32 +0400

    added current user dependency for jwt authentication logic

--- app/auth.py
+++ app/auth.py
@@ -2,9 +2,11 @@ from passlib.context import CryptContext
 from . import models
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
+from jose import jwt, JWTError
 from datetime import datetime, timedelta
 from typing import Optional
+from .database import SessionLocal
+from fastapi import HTTPException, status, Depends
 
 SECRET_KEY = "my-secret-key"  # i will replace it with env variable later
 ALGORITHM = "HS256"
@@ -24,6 +26,7 @@ def authenticate_user(db: Session, username: str, password: str):
         return False
     return user
 
+
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
@@ -33,3 +36,23 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(lambda: SessionLocal())):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Couldn't validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = db.query(models.User).filter(
+        models.User.username == username).first()
+    if user is None:
+        raise credentials_exception
+    return user
