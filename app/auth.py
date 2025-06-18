ad9df95c1d606bb826c786e6ee0065601a3b407a
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-06-18 15:12:02 +0400

    added acces token expire time constant

--- app/auth.py
+++ app/auth.py
@@ -10,6 +10,7 @@ from fastapi import HTTPException, status, Depends
 
 SECRET_KEY = "my-secret-key"  # i will replace it with env variable later
 ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
