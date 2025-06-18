adb5d3e924b152e44d8c5fe5a3526a6b5d817979
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-06-18 14:25:28 +0400

    added user authentication with database

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,17 @@
 from passlib.context import CryptContext
+from . import models
+from sqlalchemy.orm import Session
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = db.query(models.User).filter(
+        models.User.username == username).first()
+    if not user or not verify_password(password, user.password):
+        return False
+    return user
