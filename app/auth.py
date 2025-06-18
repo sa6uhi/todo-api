de3a3f04a08e3999ba8ed39ce5c9d0c99b6a01ce
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-06-18 14:35:00 +0400

    added JWT token creation with expiration logic

--- app/auth.py
+++ app/auth.py
@@ -2,6 +2,12 @@ from passlib.context import CryptContext
 from . import models
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from datetime import datetime, timedelta
+from typing import Optional
+
+SECRET_KEY = "my-secret-key"  # i will replace it with env variable later
+ALGORITHM = "HS256"
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -17,3 +23,13 @@ def authenticate_user(db: Session, username: str, password: str):
     if not user or not verify_password(password, user.password):
         return False
     return user
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(datetime.timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
