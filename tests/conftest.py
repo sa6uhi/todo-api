500b875b8402f794af6eee408ac89d57948f89d6
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-06-18 22:19:12 +0400

    added test user fixture with hashed password

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
 import pytest
+import pytest_asyncio
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 from app.main import app
 from app.database import Base
+from app import models
+from passlib.context import CryptContext
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
@@ -39,3 +42,20 @@ def client(session):
     app.dependency_overrides[app.get_db] = override_get_db
     yield TestClient(app)
     del app.dependency_overrides[app.get_db]
+
+
+@pytest_asyncio.fixture
+async def test_user(session):
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    hashed_password = pwd_context.hash("password123")
+    user_data = {
+        "first_name": "Test",
+        "last_name": "User",
+        "username": "testuser",
+        "password": hashed_password,
+    }
+    db_user = models.User(**user_data)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return user_data
