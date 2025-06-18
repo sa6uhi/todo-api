339f9125f87b0273338780107d79c613f70358d1
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-06-18 14:58:00 +0400

    added token schema for jwt authentication logic

--- app/schemas.py
+++ app/schemas.py
@@ -1,16 +1,24 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 
+
 class UserBase(BaseModel):
     first_name: str
     last_name: Optional[str] = None
     username: str
 
+
 class UserCreate(UserBase):
     password: str = Field(..., min_length=6)
+
 
 class User(UserBase):
     id: int
 
     class Config:
         from_attributes = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
