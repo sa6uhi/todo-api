4dc7989c90b125d35052450023f9dcce8c9ff032
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-06-18 15:53:20 +0400

    added TaskStatus enum for task state management

--- app/models.py
+++ app/models.py
@@ -1,5 +1,11 @@
 from sqlalchemy import Column, Integer, String, Text
 from .database import Base
+import enum
+
+class TaskStatus(enum.Enum):
+    NEW = "New"
+    IN_PROGRESS = "In progress"
+    COMPLETED = "Completed"
 
 class User(Base):
     __tablename__ = "users"
