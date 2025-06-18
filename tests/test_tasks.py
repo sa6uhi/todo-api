62de8b2ccbd9517fa2b1b32d99734dfbb1ee319b
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-06-18 23:20:39 +0400

    added task detail endpoint test

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -27,9 +27,25 @@ def test_read_tasks(client):
     assert "skip" in data
     assert "limit" in data
 
+
 def test_read_user_tasks(client, token):
-    response = client.get("/tasks/user/", headers={"Authorization": f"Bearer {token}"})
+    response = client.get(
+        "/tasks/user/", headers={"Authorization": f"Bearer {token}"})
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
     assert "items" in data
     assert "total" in data
+
+
+def test_read_task(client, token):
+    task_data = {"title": "Task to Read", "description": "Read Test"}
+    create_response = client.post(
+        "/tasks/",
+        json=task_data,
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    task_id = create_response.json()["id"]
+    response = client.get(f"/tasks/{task_id}")
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data["title"] == "Task to Read"
